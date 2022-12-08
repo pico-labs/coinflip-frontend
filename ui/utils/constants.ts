1de68e9e3152365a7191f3c74e538731681b5894
James Boyle <jhhboyle@gmail.com>
2022-12-07 20:58:03 -0600

    The deployed connection works

--- ui/utils/constants.ts
+++ ui/utils/constants.ts
@@ -5,14 +5,14 @@ const networkConfig = {
       publicKey: 'B62qiTeKV99ugy2JpAV1wGJ7cPGaUBBohH8MPjjGCVFdFexVmVyARHb',
       datastoreKey: 'berkeley-state',
     },
-    oracleUrl: undefined,
+    oracleUrl: 'https://randomness-oracle-ah5yo4vyg-pico-labs.vercel.app/api/randomNumber',
   },
   LOCAL: {
     coinflipContract: {
       publicKey: undefined,
       datastoreKey: 'local-state',
     },
-    oracleUrl: 'http://localhost:3030/randomNumber'
+    oracleUrl: 'http://localhost:3030/api/randomNumber'
   }
 }
 
