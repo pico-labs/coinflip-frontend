80bbc35d7bb6585d39fd008822c4481891a1cdcd
James Boyle <jhhboyle@gmail.com>
2022-12-07 21:09:25 -0600

    Use Berkeley by default and wrap fetch in try-catch

--- ui/utils/OracleDataSource.ts
+++ ui/utils/OracleDataSource.ts
@@ -2,9 +2,14 @@ import {networkConfig} from './constants';
 
 export class OracleDataSource {
   static async get() {
-    const url = networkConfig[networkConfig.currentNetwork].oracleUrl;
-    const result = await fetch(url);
-    const json = await result.json();
-    return json;
+    try {
+      const url = networkConfig[networkConfig.currentNetwork].oracleUrl;
+      const result = await fetch(url);
+      const json = await result.json();
+      return json;
+    } catch (err) {
+      console.error(err);
+      return {error: err};
+    }
   }
 }
